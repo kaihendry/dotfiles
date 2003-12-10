3a186754faa8cb85e39dfdd1f100b685b4c3c706
hendry <hendry@1860b75d-abcd-0310-9589-ecd4573dcfec>
2003-12-10 11:17:37 +0000

    With the help of the FAQ I hope this is the right config
    git-svn-id: http://svn.natalian.org/home@86 1860b75d-abcd-0310-9589-ecd4573dcfec

---
+++ .mozilla/default/ack41g03.slt/70572827.s
@@ -0,0 +1,16 @@
+#2c
+http://db.cs.helsinki.fi
+https://www.amazon.co.uk
+.
+http://bugs.skolelinux.no
+Bugzilla_login
+~aGVuZHJ5QGNzLmhlbHNpbmtpLmZp
+*Bugzilla_password
+~ODYxNjUx
+.
+http://bugs.skolelinux.no
+Bugzilla_login
+~aGVuZHJ5QGNzLGhlbHNpbmtpLmZp
+*Bugzilla_password
+~ODYxNjUx
+.
