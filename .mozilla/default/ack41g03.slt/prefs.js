9a1324a26191f19752c475c4dcf129636b50fbb9
hendry <hendry@1860b75d-abcd-0310-9589-ecd4573dcfec>
2004-03-20 07:36:32 +0000

    automated
    git-svn-id: http://svn.natalian.org/home@495 1860b75d-abcd-0310-9589-ecd4573dcfec

--- .mozilla/default/ack41g03.slt/prefs.js
+++ .mozilla/default/ack41g03.slt/prefs.js
@@ -42,7 +42,7 @@ user_pref("font.name.cursive.x-western", "adobe-courier-iso8859-1");
 user_pref("font.name.fantasy.x-western", "adobe-courier-iso8859-1");
 user_pref("font.size.variable.x-western", 20);
 user_pref("intl.accept_languages", "");
-user_pref("intl.charsetmenu.browser.cache", "ISO-8859-5, UTF-8, ISO-8859-1, windows-1252, ISO-8859-7");
+user_pref("intl.charsetmenu.browser.cache", "ISO-8859-15, ISO-8859-5, UTF-8, ISO-8859-1, windows-1252");
 user_pref("intl.charsetmenu.composer.cache", "ISO-8859-1");
 user_pref("ldap_2.prefs_migrated", true);
 user_pref("mailnews.quotingPrefs.version", 1);
