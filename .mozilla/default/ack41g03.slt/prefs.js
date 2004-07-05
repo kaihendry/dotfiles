132c465455f4f6febb0f9adf9d7eb680c4954045
hendry <hendry@1860b75d-abcd-0310-9589-ecd4573dcfec>
2004-07-05 15:15:23 +0000

    automated
    git-svn-id: http://svn.natalian.org/home@902 1860b75d-abcd-0310-9589-ecd4573dcfec

--- .mozilla/default/ack41g03.slt/prefs.js
+++ .mozilla/default/ack41g03.slt/prefs.js
@@ -9,13 +9,13 @@
  * For more information, see http://www.mozilla.org/unix/customizing.html#prefs
  */
 
-user_pref("browser.download.dir", "/web/prazefarm.co.uk");
+user_pref("browser.download.dir", "/tmp");
 user_pref("browser.download.save_converter_index", 0);
 user_pref("browser.history.last_page_visited", "http://www.mozilla.org/start/1.0/");
 user_pref("browser.search.defaultengine", "engine:///usr/lib/mozilla/searchplugins/google.src");
 user_pref("browser.search.opensidebarsearchpanel", false);
 user_pref("browser.startup.homepage", "http://www.cs.helsinki.fi/u/hendry/");
-user_pref("browser.startup.homepage_override.mstone", "rv:1.7");
+user_pref("browser.startup.homepage_override.mstone", "rv:1.6");
 user_pref("browser.tabs.autoHide", false);
 user_pref("browser.tabs.extensions.auto_cleanUp_count", 2);
 user_pref("browser.tabs.extensions.default.type", 0);
