59ba281d72e4595705ef609649497adf9b2aa2b3
hendry <hendry@1860b75d-abcd-0310-9589-ecd4573dcfec>
2004-07-23 15:10:59 +0000

    def winprop
    git-svn-id: http://svn.natalian.org/home@974 1860b75d-abcd-0310-9589-ecd4573dcfec

--- .ion3/browserprop.lua
+++ .ion3/browserprop.lua
@@ -1,7 +1,4 @@
--- Ion startup error log:
--- >> /home/hendry/.ion3/browserprop.lua:2: attempt to call global `winprop' (a nil value)
--- xprop WM_CLASS
-winprop {
+defwinprop {
    class = "Mozilla-bin", 
    instance = "Gecko",
    target = "web"
--- .ion3/ion.lua
+++ .ion3/ion.lua
@@ -57,14 +57,4 @@ ioncore.load_module("mod_ionws")
 --ioncore.load_module("mod_sp")
 
 -- For detailing which workspace my browser should go
--- include("browserprop")
-
-
--- Ion startup error log:
--- >> /home/hendry/.ion3/browserprop.lua:2: attempt to call global `winprop' (a nil value)
--- xprop WM_CLASS
-winprop {
-   class = "Mozilla-bin", 
-   instance = "Gecko",
-   target = "web",
-}
+include("browserprop")
