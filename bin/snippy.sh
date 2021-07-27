acfee118558f4307f3fdeef159373c814984641f
Kai Hendry <kai.hendry@motional.com>
2021-07-27 10:02:42 +0800

    From https://youtu.be/PRgIxRl67bk

---
+++ bin/snippy.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+SNIPS=${HOME}/snippets
+
+FILE=`ls ${SNIPS} | /usr/bin/rofi -dmenu`
+
+if [ -f ${SNIPS}/${FILE} ]; then
+  DATA=$([ -x "${SNIPS}/${FILE}" ] && bash "${SNIPS}/${FILE}" || head --bytes=-1 ${SNIPS}/${FILE})
+  printf "$DATA" | xsel -p -i 
+  printf "$DATA" | xsel -b -i 
+  xdotool key shift+Insert
+fi
