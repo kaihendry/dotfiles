a26fc1a5e4d366b86f533070a5740823b6f03311
Kai Hendry <kai.hendry@motional.com>
2021-11-07 14:53:33 +0800

    gopls !

--- .config/nvim/lua/lsp.lua
+++ .config/nvim/lua/lsp.lua
@@ -39,7 +39,10 @@ capabilities = require('cmp_nvim_lsp').update_capabilities(capabilities)
 
 -- Use a loop to conveniently call 'setup' on multiple servers and
 -- map buffer local keybindings when the language server attaches
+
+require'lspconfig'.gopls.setup{}
 local servers = { 'tsserver' }
+
 for _, lsp in ipairs(servers) do
   nvim_lsp[lsp].setup {
     on_attach = on_attach,
--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -3,9 +3,6 @@ vim.cmd [[packadd packer.nvim]]
 return require('packer').startup(function()
 	use {'dracula/vim', as = 'dracula'}
 	use 'tpope/vim-commentary' -- "gc" to comment visual regions/lines
-	-- use 'nvim-treesitter/nvim-treesitter' -- better syntax highlighting
-	-- -- Additional textobjects for treesitter
-	-- use 'nvim-treesitter/nvim-treesitter-textobjects'
 	use 'neovim/nvim-lspconfig' -- use a language server
 	use 'hrsh7th/cmp-nvim-lsp'
 	use 'hrsh7th/cmp-buffer'
@@ -14,4 +11,8 @@ return require('packer').startup(function()
 	-- use 'hrsh7th/cmp-copilot'
 	use 'saadparwaiz1/cmp_luasnip' -- vim cmp requires luasnip
     use 'L3MON4D3/LuaSnip' -- Snippets plugin
+
+    use 'crispgm/nvim-go'
+	use 'nvim-lua/plenary.nvim'
+    use 'nvim-lua/popup.nvim'
 end)
